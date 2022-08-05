6af7888f55aee3231a820f745f44cf19c49c6ee5
Diego Leandro <d.deleon17@unapec.edu.do>
2022-08-05 14:57:39 -0400

    V01

---
+++ js/mainScript.js
@@ -0,0 +1,117 @@
+// Funcionalidad para la barra de navegacion y los botonos que conlleva 
+
+let navbar = document.querySelector('.navbar')
+let login = document.querySelector('.login-form')
+let signUp = document.querySelector('.signup-form')
+
+
+document.querySelector('#menu-btn').onclick = () => {
+    navbar.classList.toggle('active');
+    login.classList.remove('active');
+    signUp.classList.remove('active');
+}
+
+document.querySelector('#login-btn').onclick = () => {
+    login.classList.toggle('active');
+    navbar.classList.remove('active');
+    signUp.classList.remove('active');
+}
+
+document.querySelector('#create').onclick = () => {
+    signUp.classList.toggle('active');
+    login.classList.remove('active');
+}
+
+document.querySelector('#login-back').onclick = () => {
+    login.classList.toggle('active');
+    signUp.classList.remove('active');
+}
+
+
+Window.onscroll = () => {
+    navbar.classList.remove('active');
+    login.classList.remove('active');
+    signUp.classList.remove('active');
+}
+
+
+// Funcionalidad de tema oscuro
+
+let themeBtn = document.querySelector('#theme-btn');
+
+themeBtn.onclick = () =>{
+    themeBtn.classList.toggle('fa-sun');
+
+    if (themeBtn.classList.contains('fa-sun')) {
+        document.body.classList.add('active');
+    }else{
+        document.body.classList.remove('active');
+    }
+};
+
+// funcionalidad y aspectos sobre la seccion de reviews
+
+var swiper = new Swiper(".review-slider", {
+    loop: true,
+    spaceBetween: 30,
+    centerSlides: true,
+    autoplay:{
+        delay: 5500,
+        disableOnInteraction: false,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      dynamicBullets: true,
+    },
+});
+
+// Funcion para desplegar mensaje que pida codigo para entrar en la pagina de administrador
+
+function adminCode(){
+    var codeAdmin = prompt('Insert the administrator code'); 
+
+    if(codeAdmin == "admin01"){
+        alert("Access granted, Welcome Administrator");
+        window.location.href = "administrator.html";
+     }else{
+        alert('Invalid code, try again');
+        return null;
+    }
+}
+
+// Funcion para desplegar un mensaje de envio correcto de comentario 
+
+function confirmContacting(){
+    confirm("Your comment has been sent successfully, Thanks for the feedback :) ");
+    window.location.href = "home.html";
+}
+
+// Funcion de redireccionamiento de boton hacia pagina de tickets
+
+function sendToTickets(){
+    window.location.href = "tickets.html";
+}
+
+// Funcion de mensaje de añadido realizado correctamente 
+function changeSaved(){
+    confirm("Changes have been made correctly");
+    window.location.href = "administrator.html"
+}
+
+
+
+// // function on word rotation 
+
+// setInterval(function() {
+//     const show = document.querySelector('span[data-show]')
+//     const next = show.nextElementSibling || document.querySelector('span:first-child')
+//     const up = document.querySelector('span[data-up]')
+
+//     if(up){
+//         up.removeAttribute('data-up')
+//     }
+
+//     show.removeAttribute('data-show')
+//     show.setAttribute('data-up', '')
+//     next.setAttribute('data-show', '')
+// }, 2000); 
---
+++ js/ticketScript.js
@@ -0,0 +1,33 @@
+let cards = document.querySelector('.offers');
+let checkout = document.querySelector('.container');
+let boardingPass = document.querySelector('.boarding-pass') 
+
+
+// let list = document.querySelectorAll('.btn');
+// for (i=0; i<list.length; i++){
+//     list[i].onclick = function(){
+//     list[i].addEventListener('click', function(){
+//         checkout.classList.toggle('on');
+//     })
+//     }
+// }
+
+
+document.querySelector('#prueba').onclick = () => {
+    checkout.classList.toggle('on');
+    cards.classList.toggle('off');
+}
+
+document.getElementById("forms").addEventListener('submit', function(e){
+    e.preventDefault(); 
+})
+
+document.querySelector('#proceedToBoard').onclick = () =>{
+    boardingPass.classList.toggle('give'); 
+    checkout.classList.remove('on');
+}
+
+let navbar = document.querySelector('.navbar');
+document.querySelector('#menu-btn').onclick = () => {
+    navbar.classList.toggle('active');
+}
